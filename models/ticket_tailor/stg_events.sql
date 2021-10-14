570f8c97e9c41f5c7c08aaed120e1d968d192f0c
wasila.quader@dbtlabs.com <wasila.quader@dbtlabs.com>
2021-10-14 18:14:37 +0000

    build staging model "events"

---
+++ models/ticket_tailor/stg_events.sql
@@ -0,0 +1,26 @@
+with events as (
+
+    select
+      id as event_id,
+      name as event_name,
+      status,
+      private,
+      "START":unix::timestamp as start_time,
+      "END":unix::timestamp as end_time,
+      timezone,
+      online_event,
+      venue:name::string as venue,
+      venue:postal_code::int as postal_code,
+      tickets_available,
+      total_issued_tickets,
+      total_orders,
+      ticket_types[0]:price::int as ticket_price,
+      ticket_types[0]:status::string as ticket_status,
+      ticket_types[0]:quantity::int as remaining_tickets,
+      ticket_types[0]:max_per_order::int as max_tickets_per_order,
+      ticket_types[0]:min_per_order::int as min_tickets_per_order
+    from {{ source('ticket_tailor', 'events') }}
+
+)
+
+select * from events
