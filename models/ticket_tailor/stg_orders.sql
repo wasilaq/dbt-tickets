a5c7c3d905946ccb17e1e6371878c367c35dd6e5
wasila.quader@dbtlabs.com <wasila.quader@dbtlabs.com>
2021-10-14 18:41:17 +0000

    remove events information from "stg_orders"

--- models/ticket_tailor/stg_orders.sql
+++ models/ticket_tailor/stg_orders.sql
@@ -11,10 +11,7 @@ with orders as (
     refund_amount,
     currency:code::string as currency,
     line_items[0]:type::string as item_type,
-    event_summary:id::string as event_id,
-    event_summary:name::string as event_name,
-    event_summary:start_date.unix::timestamp as start_date,
-    event_summary:end_date.unix::timestamp as end_date
+    event_summary:id::string as event_id
   from {{ source('ticket_tailor', 'orders') }}
 
 )
