83159d6b421da96f40b7c57913525c7cd2563d6a
wasila.quader@dbtlabs.com <wasila.quader@dbtlabs.com>
2021-10-20 19:59:39 +0000

    include all columns in stg_issued_tickets

--- models/staging/ticket_tailor/stg_issued_tickets.sql
+++ models/staging/ticket_tailor/stg_issued_tickets.sql
@@ -1,12 +1,25 @@
 with tickets as (
-    select
-      id as ticket_id,
-      event_id,
-      order_id,
-      case when status = 'valid' then 'purchased'
-      when status = 'voided' then 'returned'
-      end as status
-    from {{ source('ticket_tailor', 'issued_tickets') }}
+
+  select * from {{ source('ticket_tailor', 'issued_tickets') }}
+
+),
+
+final as (
+
+  select
+    id as ticket_id,
+    event_id,
+    order_id,
+    ticket_type_id,
+    barcode,
+    barcode_url,
+    created_at::timestamp as created_at,
+    status,
+    updated_at::timestamp as updated_at,
+    voided_at::timestamp as voided_at,
+    _sdc_batched_at
+  from tickets
+
 )
 
-select * from tickets
+select * from final
