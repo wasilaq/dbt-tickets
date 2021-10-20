24f600fc9933a6c2ec808557fd7f1492f8a58b6d
wasila.quader@dbtlabs.com <wasila.quader@dbtlabs.com>
2021-10-20 20:21:52 +0000

    correct sql syntax (stg_orders)

--- models/staging/ticket_tailor/stg_orders.sql
+++ models/staging/ticket_tailor/stg_orders.sql
@@ -18,11 +18,11 @@ final as (
     txn_id,
     refund_amount,
     currency:code::string as currency,
-    currency:base_multiplier as currency_base_multiplier
+    currency:base_multiplier as currency_base_multiplier,
     line_items[0]:type::string as line_item_type,
     line_items[0]:description as line_item_description,
     line_items[0]:id as line_item_id,
-    line_items[0]:object as line_item_object
+    line_items[0]:object as line_item_object,
     event_summary:id::string as event_id,
     event_summary:name as event_name,
     event_summary:start_date.unix::timestamp as event_start_time,
